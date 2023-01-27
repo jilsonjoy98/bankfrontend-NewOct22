af3c854480fbab012b007e82d410eecec11a2fbe
jilsonjoy98 <jilsonjoy98@gmail.com>
2023-01-27 16:19:35 +0530

    Bank front end complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RegisterComponent } from './register/register.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //login
+  {
+    path:'' ,component:LoginComponent
+},
+// register
+{
+  path:'register',component:RegisterComponent
+},
+// Dashboard
+{
+  path:'dashboard',component:DashboardComponent
+},
+// TransactionsComponent
+{
+  path:'transactions',component:TransactionsComponent
+},
+// PageNotFoundComponent
+{
+  path:'**', component:PageNotFoundComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
